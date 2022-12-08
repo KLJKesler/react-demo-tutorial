e63502945d1f375261c616231608aed11ffd54ce
Keaneux Kesler <keaneuxkesler@gmail.com>
2022-12-08 23:24:38 +0000

    My first component and CSS styling

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+const ExpenseItem = () => {
+    return (
+        <div className='expense-item'>
+            <div>Date: March 28th 2022</div>
+            <div className='expense-item__description'>
+                <h2>Title: Car Insurance</h2>
+                <div className='expense-item__price'>Amount: $294.67</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
