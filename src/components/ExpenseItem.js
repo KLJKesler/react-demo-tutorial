6f5ea32a33c1dff265350e272b44f131c56c2638
Keaneux Kesler <keaneuxkesler@gmail.com>
2022-12-11 13:38:10 +0000

    Adding dynamic data with props in React JS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
     return (
         <div className='expense-item'>
-            <div>Date: March 28th 2022</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>Title: Car Insurance</h2>
-                <div className='expense-item__price'>Amount: $294.67</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
